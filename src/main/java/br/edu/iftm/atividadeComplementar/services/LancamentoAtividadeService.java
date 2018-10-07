cbbc10b88539822516907bafc8fc431d162637c5
robertoeugenio1978@gmail.com <robertoeugenio>
2018-10-07 17:31:56 -0300

    LancamentoAtividade service

---
+++ src/main/java/br/edu/iftm/atividadeComplementar/services/LancamentoAtividadeService.java
@@ -0,0 +1,30 @@
+package br.edu.iftm.atividadeComplementar.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.iftm.atividadeComplementar.domains.LancamentoAtividade;
+import br.edu.iftm.atividadeComplementar.repositories.AtividadeRepository;
+
+@Service
+public class LancamentoAtividadeService {
+	
+	@Autowired
+	private AtividadeRepository repository;
+	
+	public List<LancamentoAtividade> buscar(Integer codigo) {
+		return repository.findByNomeContaining(codigo);
+	}
+	
+	public Optional<LancamentoAtividade> buscarRa(Integer codigo) {
+		return repository.findById(codigo);
+	}
+	
+	public List<LancamentoAtividade> buscarTodos() {
+		return repository.findAll();
+	}
+	
+}
