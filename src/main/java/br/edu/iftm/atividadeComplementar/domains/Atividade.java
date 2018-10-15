4099fc40f3fcee25ed012fc24e7c0306e60df103
robertoeugenio1978@gmail.com <robertoeugenio>
2018-10-15 00:10:08 -0300

    mapeando

--- src/main/java/br/edu/iftm/atividadeComplementar/domains/Atividade.java
+++ src/main/java/br/edu/iftm/atividadeComplementar/domains/Atividade.java
@@ -2,24 +2,35 @@ package br.edu.iftm.atividadeComplementar.domains;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import javassist.SerialVersionUID;
 
 @Entity
+@Table(name="Tabela_Atividade")
 public class Atividade implements Serializable{
+	private static final long SerialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="Codigo_Atividade")
 	private Integer codigo;
 	
+	@Column(name="Nome_Atividade",length=255, nullable=false, unique=true)
 	private String nome;
 	
+	@Column(name="Percentual_CargaHoraria")
 	private Integer percentualCargaHoraria;
 	
+	@Column(name="Maximo_AtividadeCodigoSemestre")
 	private Integer maximoAtivcodigoadesSemestre;
-	
+
+	@Column(name="Percentual_porAtividadeCodigoSemestre")
 	private Integer percentualPorAtivcodigoade;
 
 	public Atividade(Integer codigo, String nome, Integer percentualCargaHoraria, Integer maximoAtivcodigoadesSemestre,
